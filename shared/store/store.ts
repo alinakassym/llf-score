acb42f47085c676276ad1675426397993c4432aa
alinakassym <alinakassym@gmail.com>
2025-08-19 01:26:54 +0500

    chore(store): register ui reducer in configureStore

---
+++ shared/store/store.ts
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit';
+import ui from './ui.slice';
+
+export const store = configureStore({
+  reducer: { ui },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
