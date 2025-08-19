9768bc26162a520557f862abb177bafc16a1392b
alinakassym <alinakassym@gmail.com>
2025-08-19 17:56:18 +0500

    feat(mock): add leagues dataset

---
+++ shared/mocks/leagues.ts
@@ -0,0 +1,8 @@
+export type League = { id: string; name: string };
+export const leagues: League[] = [
+  { id: 'pl', name: 'Премьер лига' },
+  { id: 'fl', name: 'Мастер лига' },
+  { id: 'al', name: 'Лига А' },
+  { id: 'bl', name: 'Лига B' },
+  { id: 'cl', name: 'Лига C' },
+];
