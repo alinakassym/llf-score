9443343488770d01925f2398fe9e89e50ac9b198
alinakassym <alinakassym@gmail.com>
2025-09-12 20:41:12 +0500

    feat(topBar): fetch leagues

--- features/LeaguePicker.tsx
+++ features/LeaguePicker.tsx
@@ -1,5 +1,17 @@
-import DrawerPicker from "@/shared/ui/DrawerPicker";
-import { leagues } from "@/shared/mocks/leagues";
+import React, { useEffect, useMemo } from "react";
+import DrawerPicker, {
+  DrawerItem,
+  DrawerPickerSkeleton,
+} from "@/shared/ui/DrawerPicker";
+import { leagues as mockLeagues } from "@/shared/mocks/leagues";
+import { useAppSelector, useAppDispatch } from "@/shared/store/hooks";
+import {
+  League,
+  fetchLeaguesByCityId,
+  selectLeagues,
+  selectLeaguesStatus,
+  selectCurrentCityId,
+} from "@/shared/store/leagues.slice";
 
 export default function LeaguePicker(props: {
   value?: string;
@@ -7,7 +19,30 @@ export default function LeaguePicker(props: {
   open?: boolean;
   onOpenChange?: (open: boolean) => void;
 }) {
-  const items = leagues.map((l) => ({ id: l.id, label: l.name }));
+  const dispatch = useAppDispatch();
+  const leagues: League[] = useAppSelector(selectLeagues);
+  const status = useAppSelector(selectLeaguesStatus);
+  const cityId: string = useAppSelector(selectCurrentCityId) ?? "";
+
+  useEffect(() => {
+    if (cityId !== "" && status === "idle")
+      dispatch(fetchLeaguesByCityId(cityId));
+  }, [dispatch, status, cityId]);
+
+  const items: DrawerItem[] = useMemo(
+    () =>
+      (leagues.length ? leagues : mockLeagues).map((c) => ({
+        label: c.name,
+        id: c.id.toString(),
+        icon: c?.icon,
+      })),
+    [leagues],
+  );
+
+  if (status === "loading") {
+    return <DrawerPickerSkeleton label="Город" />;
+  }
+
   return (
     <DrawerPicker
       items={items}
